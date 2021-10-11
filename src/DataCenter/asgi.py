3c9825571ee5480359c70c9076a9d5ae02b26d36
E2029-Aydin <aydinsanoz2@gmail.com>
2021-10-12 00:06:41 +0300

    venv, project, superuser created

--- src/DataCenter/asgi.py
+++ src/DataCenter/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hairPlant project.
+ASGI config for DataCenter project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hairPlant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DataCenter.settings')
 
 application = get_asgi_application()
